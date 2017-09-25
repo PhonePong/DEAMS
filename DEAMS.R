eb091210c7324dc5703fe3b9b421752fd54aff9a
Jim <rodriguezjimmy88@gmail.com>
2017-09-25 04:44:51 -0400

    added some cleaning functions

--- DEAMS.R
+++ DEAMS.R
@@ -1,6 +1,9 @@
 library(xlsx)
 
-
-
-
-AllData <- read.xlsx(filePath, 1, startRow = 5, colIndex = c(1, 4, 6, 7, 9))
+# Cleaning the data
+AllData <- read.xlsx(filePath, 1, startRow = 6, colIndex = c(1, 4, 5, 6, 7, 9))
+AllData$Type <- gsub('DEAMS - ', '', AllData$Type)
+AllData$Severity <- gsub('Severity ', '', AllData$Severity)
+AllData <- AllData[order(AllData$Submit.Date), ]
+# Regex to get the locations
+AllData$Location <- trimws(sapply(regmatches(AllData$Description, gregexpr("(?<=Base:).*?(?=\\w).*?(?=(\\s{2,3})|((I|i)ssue:))", AllData$Description, perl = TRUE)), '[',1))
